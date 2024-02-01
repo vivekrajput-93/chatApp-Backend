b5f394a5e209f5f07941e310a87c4c1c6c68e625
vivekrajput-93 <viveksinghraajputt@gmail.com>
2024-02-01 13:16:22 +0530

    added duplex connection b/n socket

--- client/script.js
+++ client/script.js
@@ -1,14 +1,19 @@
 var socket = io();
 
 let btn = document.getElementById('btn');
-btn.onclick = function exec  ()  {
-    socket.emit('from_client')
+let inputMsg = document.getElementById('newmsg');
+let msglist = document.getElementById('msgList');
+
+btn.onclick = function exec() {
+    socket.emit('msg_send', {
+        msg : inputMsg.value
+    });
+
 }
 
-socket.on('from_server', () => {
-    console.log("collected a new from server");
-    const div = document.createElement('div');
-    div.innerText = "New event from server";
-    console.log(div);
-    document.body.appendChild(div)
+
+socket.on('msg_revcd', (data) => {
+    let listMsg = document.createElement('li');
+    listMsg.innerText = data.msg;
+    msglist.appendChild(listMsg);
 })
--- index.js
+++ index.js
@@ -10,13 +10,11 @@ const  io = socketio(server);
 io.on('connection', (socket) => {
     console.log("A user is connected!");
 
-    socket.on('from_client', () => {
-        console.log("New event from client")
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        io.emit('msg_revcd', data)
     })
 
-    setInterval(() => {
-        socket.emit('from_server')
-    }, 2000)
 })
 
 app.use('/', express.static(__dirname + "/client"))
